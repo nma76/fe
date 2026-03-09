0926205b99084121bd235435b5321826f05630ca
Jonas Lindau <jonas@jonika.nu>
2026-03-09 08:17:22 +0100

    Updated i few nerd glyphs

--- internal/icon/icon_common.go
+++ internal/icon/icon_common.go
@@ -12,16 +12,16 @@ func (DefaultIconProvider) ForExecutable(f os.FileInfo) string {
 	// Check for shebang first
 	if sb, _ := readShebang(f); sb != "" {
 		if strings.Contains(sb, "python") {
-			return "\U000f0818 " // Python
+			return "\U0000e73c " // Python
 		}
 		if strings.Contains(sb, "bash") || strings.Contains(sb, "sh") {
-			return "\U000f0bc1 " // Shell
+			return "\U000f1183 " // Shell
 		}
 		if strings.Contains(sb, "node") {
-			return "\U000e7188 " // Node
+			return "\U0000ed0d " // Node
 		}
 		if strings.Contains(sb, "perl") {
-			return "\U000f03f8 " // Perl
+			return "\U0000e67e " // Perl
 		}
 		// fler senare…
 	}
@@ -30,11 +30,11 @@ func (DefaultIconProvider) ForExecutable(f os.FileInfo) string {
 	ext := strings.ToLower(filepath.Ext(f.Name()))
 	switch ext {
 	case ".app", ".exe", ".com":
-		return "\U000f107b" + " "
+		return "\U000f107b "
 	case ".sh", ".bat", ".cmd", ".ps1":
-		return "\U000f0bc1" + " "
+		return "\U000f0bc1 "
 	}
 
 	// Executable generic icon
-	return "\U000f107b" + " "
+	return "\U000f107b "
 }
