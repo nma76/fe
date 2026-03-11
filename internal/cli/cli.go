8bd68c962dee8a56f8fe455dc9aeddf354ded873
Jonas Lindau <jonas@jonika.nu>
2026-03-11 19:42:24 +0100

    Removed --path to make use simpler

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -14,22 +14,25 @@ type Options struct {
 func Parse() Options {
 	var opts Options
 
-	// path flag for the directory to scan for executables, defaulting to the current directory
-	flag.StringVar(&opts.Path, "path", ".", "Directory to scan for executables")
-	flag.StringVar(&opts.Path, "p", ".", "shorthand for --path")
-
 	// help flag to show the help message
 	flag.BoolVar(&opts.Help, "help", false, "Show help message")
 	flag.BoolVar(&opts.Help, "h", false, "shorthand for --help")
 
-	// debug flag to enable debug output
-	flag.BoolVar(&opts.Debug, "debug", false, "Enable debug output")
-
 	// icon flag to enable icon output
 	flag.BoolVar(&opts.Icon, "icon", false, "Enable icon output")
+	flag.BoolVar(&opts.Icon, "i", false, "shorthand for --icon")
+
+	// debug flag to enable debug output
+	flag.BoolVar(&opts.Debug, "debug", false, "Enable debug output")
+	flag.BoolVar(&opts.Debug, "d", false, "shorthand for --debug")
 
 	// Parse the command-line flags
 	flag.Parse()
+
+	// If user provided a positional argument, treat it as the path
+	if flag.NArg() > 0 {
+		opts.Path = flag.Arg(0)
+	}
 
 	return opts
 }
