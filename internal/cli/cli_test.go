9536039ddb03aff51f2c170ffa8516a23b46dcc9
Jonas Lindau <jonas@jonika.nu>
2026-03-12 19:32:34 +0100

    Added tests for cli

---
+++ internal/cli/cli_test.go
@@ -0,0 +1,105 @@
+package cli_test
+
+import (
+	"fe/internal/cli"
+	"flag"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func withArgs(args []string, fn func()) {
+	origArgs := os.Args
+	origCommandLine := flag.CommandLine
+
+	os.Args = args
+	flag.CommandLine = flag.NewFlagSet(args[0], flag.ExitOnError)
+
+	defer func() {
+		os.Args = origArgs
+		flag.CommandLine = origCommandLine
+	}()
+
+	fn()
+}
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+		want cli.Options
+	}{
+		{
+			name: "default values",
+			args: []string{"fe"},
+			want: cli.Options{
+				Path:  ".",
+				Help:  false,
+				Debug: false,
+				Icon:  false,
+			},
+		},
+		{
+			name: "with path argument",
+			args: []string{"fe", "/test/path"},
+			want: cli.Options{
+				Path:  "/test/path",
+				Help:  false,
+				Debug: false,
+				Icon:  false,
+			},
+		},
+		{
+			name: "with help argument",
+			args: []string{"fe", "--help"},
+			want: cli.Options{
+				Path:  ".",
+				Help:  true,
+				Debug: false,
+				Icon:  false,
+			},
+		},
+		{
+			name: "with icon argument",
+			args: []string{"fe", "--icon"},
+			want: cli.Options{
+				Path:  ".",
+				Help:  false,
+				Debug: false,
+				Icon:  true,
+			},
+		},
+		{
+			name: "with debug argument",
+			args: []string{"fe", "--debug"},
+			want: cli.Options{
+				Path:  ".",
+				Help:  false,
+				Debug: true,
+				Icon:  false,
+			},
+		},
+		{
+			name: "with arguments and path",
+			args: []string{"fe", "-i", "/test/path"},
+			want: cli.Options{
+				Path:  "/test/path",
+				Help:  false,
+				Debug: false,
+				Icon:  true,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			withArgs(tt.args, func() {
+				got := cli.Parse()
+
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("Parse() = %v, want %v", got, tt.want)
+				}
+			})
+		})
+	}
+}
